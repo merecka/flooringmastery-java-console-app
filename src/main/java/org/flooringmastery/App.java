d5c2ded10d799e858c7c13b74368518d4de547f2
alex-merecka <amerecka@webcreek.com>
2022-08-16 16:33:25 -0500

    Initialized project with Spring as dependency.

---
+++ src/main/java/org/flooringmastery/App.java
@@ -0,0 +1,11 @@
+package org.flooringmastery;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        AnnotationConfigApplicationContext appContext = new AnnotationConfigApplicationContext();
+    }
+}
