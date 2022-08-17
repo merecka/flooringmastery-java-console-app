559916209811319f9380c7810b23b832bbe8a0a1
alex-merecka <amerecka@webcreek.com>
2022-08-17 10:06:37 -0500

    Added Main Menu.

--- src/main/java/org/flooringmastery/controller/FlooringMasteryController.java
+++ src/main/java/org/flooringmastery/controller/FlooringMasteryController.java
@@ -1,4 +1,54 @@
 package org.flooringmastery.controller;
 
+import org.flooringmastery.ui.FlooringMasteryView;
+
 public class FlooringMasteryController {
+
+    private FlooringMasteryView view;
+
+    public FlooringMasteryController(FlooringMasteryView view) {
+        this.view = view;
+    }
+
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection;
+
+        while (keepGoing) {
+
+            menuSelection = getMenuSelection();
+
+            switch (menuSelection) {
+                case 1:
+//                    displayOrders();
+                    break;
+                case 2:
+//                    addOrder();
+                    break;
+                case 3:
+//                    editOrder();
+                    break;
+                case 4:
+//                    deleteOrder();
+                    break;
+                case 5:
+                    System.out.println("Feature not enabled yet.");
+                    break;
+                case 6:
+                    keepGoing = false;
+                    break;
+                default:
+//                    unknownCommand();
+            }
+        }
+        exitMessage();
+    }
+
+    private int getMenuSelection() {
+        return view.printMenuAndGetSelection();
+    }
+
+    private void exitMessage() {
+        view.displayExitBanner();
+    }
 }
--- src/main/java/org/flooringmastery/ui/FlooringMasteryView.java
+++ src/main/java/org/flooringmastery/ui/FlooringMasteryView.java
@@ -1,4 +1,27 @@
 package org.flooringmastery.ui;
 
 public class FlooringMasteryView {
+
+    private UserIO io;
+
+    public FlooringMasteryView(UserIO userIO) {
+        this.io = userIO;
+    }
+
+    public int printMenuAndGetSelection() {
+        io.print("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+        io.print("* <<Flooring Program>>");
+        io.print("* 1. Display Orders");
+        io.print("* 2. Add an Order");
+        io.print("* 3. Edit an Order");
+        io.print("* 4. Remove an Order");
+        io.print("* 5. Export All Data");
+        io.print("* 6. Quit");
+
+        return io.readInt("Please select from the above six Options (1-6).", 1, 6);
+    }
+
+    public void displayExitBanner() {
+        io.print("Have a good day!!");
+    }
 }
