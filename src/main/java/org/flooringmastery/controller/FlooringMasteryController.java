5739dfbfe01192ee02bc8bd484551ce74f83c112
alex-merecka <amerecka@webcreek.com>
2022-08-17 16:57:56 -0500

    Implemented the Display All Orders functionality.

--- src/main/java/org/flooringmastery/controller/FlooringMasteryController.java
+++ src/main/java/org/flooringmastery/controller/FlooringMasteryController.java
@@ -1,16 +1,24 @@
 package org.flooringmastery.controller;
 
+import org.flooringmastery.dao.FlooringMasteryPersistenceException;
+import org.flooringmastery.dto.Order;
+import org.flooringmastery.service.FlooringMasteryServiceLayer;
 import org.flooringmastery.ui.FlooringMasteryView;
+
+import java.util.List;
 
 public class FlooringMasteryController {
 
     private FlooringMasteryView view;
 
-    public FlooringMasteryController(FlooringMasteryView view) {
+    private FlooringMasteryServiceLayer service;
+
+    public FlooringMasteryController(FlooringMasteryView view, FlooringMasteryServiceLayer service) {
         this.view = view;
+        this.service = service;
     }
 
-    public void run() {
+    public void run() throws FlooringMasteryPersistenceException {
         boolean keepGoing = true;
         int menuSelection;
 
@@ -20,7 +28,7 @@ public class FlooringMasteryController {
 
             switch (menuSelection) {
                 case 1:
-//                    displayOrders();
+                    displayOrders();
                     break;
                 case 2:
 //                    addOrder();
@@ -46,6 +54,12 @@ public class FlooringMasteryController {
 
     private int getMenuSelection() {
         return view.printMenuAndGetSelection();
+    }
+
+    private void displayOrders() throws FlooringMasteryPersistenceException {
+        String orderDateToDisplayString = view.displayOrdersPrompt();
+        List<Order> allOrders =  service.allOrdersForDate(orderDateToDisplayString);
+        view.displayAllOrders(allOrders, orderDateToDisplayString);
     }
 
     private void exitMessage() {
--- src/main/java/org/flooringmastery/ui/FlooringMasteryView.java
+++ src/main/java/org/flooringmastery/ui/FlooringMasteryView.java
@@ -1,5 +1,11 @@
 package org.flooringmastery.ui;
 
+import org.flooringmastery.dto.Order;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FlooringMasteryView {
 
     private UserIO io;
@@ -19,6 +25,55 @@ public class FlooringMasteryView {
         io.print("* 6. Quit");
 
         return io.readInt("Please select from the above six Options (1-6).", 1, 6);
+    }
+
+    public String displayOrdersPrompt() {
+        io.print("* * * * Display Orders * * * * * * *");
+        String userEnteredDate;
+        boolean keepGoing = false;
+        do {
+            userEnteredDate = io.readString("Please enter an Order date that you would like view Orders for. (ex: 08/03/2022)").trim();
+
+            final String regex = "\\A[0-1][1-9]/[0-1][1-9]/20\\d\\d\\z";
+
+            final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+            final Matcher matcher = pattern.matcher(userEnteredDate);
+            boolean matchFound = matcher.find();
+
+            if (matchFound) {
+                keepGoing = false;
+            } else {
+                System.out.println("Invalid date.  Please use the correct date format.");
+                keepGoing = true;
+            }
+        } while (keepGoing);
+        return userEnteredDate;
+    }
+
+    public void displayAllOrders(List<Order> allOrders, String orderDate) {
+        io.print("* * * * Orders for " + orderDate + " * * * * * * *");
+        for (Order currentOrder : allOrders) {
+            String orderInfo = String.format("Order Number: %s - Customer: %s - State: %s - Tax Rate: %s - Product Type: %s - Area: %s - Cost: $%s sq/ft - Labor $%s sq/ft - Material $%s - Labor $%s - Tax: $%s - Total: $%s",
+                    currentOrder.getOrderNumber(),
+                    currentOrder.getCustomerName(),
+                    currentOrder.getState(),
+                    currentOrder.getTaxRate(),
+                    currentOrder.getProductType(),
+                    currentOrder.getArea(),
+                    currentOrder.getCostPerSquareFoot(),
+                    currentOrder.getLaborCostPerSquareFoot(),
+                    currentOrder.getMaterialCost(),
+                    currentOrder.getLaborCost(),
+                    currentOrder.getTax(),
+                    currentOrder.getTotal());
+            io.print(orderInfo);
+        }
+    }
+
+    public void displayErrorMessage(String errorMsg) {
+        io.print("=== ERROR ===");
+        io.print(errorMsg);
+        io.print("");
     }
 
     public void displayExitBanner() {
